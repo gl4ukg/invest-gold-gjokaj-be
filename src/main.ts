f7c8e02104c73966737272de15b26c2114497591
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-06-02 23:45:53 +0400

    log added

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.enableCors();
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(3000, '0.0.0.0'); 
+  await app.listen(3000, '0.0.0.0');
+  console.log('Application is running on: http://localhost:3000');
 }
 bootstrap();
