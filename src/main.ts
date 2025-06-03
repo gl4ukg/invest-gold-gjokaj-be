82f2e5ef4b20ae134232a6925b6c6030d1854680
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-06-03 18:05:02 +0400

    vers 2

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3000, '0.0.0.0');
-  console.log('Application is running on: http://localhost:3000');
+  console.log('Application is running on: http://localhost:3000 2');
 }
 bootstrap();
