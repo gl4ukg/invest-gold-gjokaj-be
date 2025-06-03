9f0da564f9fac05c91c52d7ac95e40ff9ed6b873
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-06-03 20:05:59 +0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3002', 'https://invest-gold-gjokaj-fe.vercel.app', 'https://investgoldgjokaj.com'],
+    credentials: true,
+  });
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3000, '0.0.0.0');
