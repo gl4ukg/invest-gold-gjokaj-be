e69776adbce250674a5a85fd9a6abf2d041e774d
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-06-17 19:40:36 +0400

    compression

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
     origin: ['http://localhost:3002', 'https://invest-gold-gjokaj-fe.vercel.app', 'https://investgoldgjokaj.com', 'https://invest-gold-gjokaj-fe-gl4ukgs-projects.vercel.app', 'https://www.investgoldgjokaj.com'],
     credentials: true,
   });
+  app.use(compression());
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3000, '0.0.0.0');
